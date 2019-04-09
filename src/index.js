76d5f158bee5a9a9a095fcd1a95edc3bd5eb63cb
Waga Odongo <wagaodongo@gmail.com>
2019-04-09 10:16:29 +0300

    amplify graphql
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import Amplify from "aws-amplify";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
