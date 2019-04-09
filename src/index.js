6de84de5646c7e859f741f47ddd674ebc3df876f
Waga Odongo <wagaodongo@gmail.com>
2019-04-09 10:20:50 +0300

    ignored files
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 import Amplify from "aws-amplify";
+import aws_exports from "./aws-exports";
+
+Amplify.configure(aws_exports);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
