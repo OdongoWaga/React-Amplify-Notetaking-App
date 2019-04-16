863d38fa84d23e960ae5ddd555eebe69539bae31
Waga Odongo <wagaodongo@gmail.com>
2019-04-16 15:23:28 +0300

    App Page
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,8 +2,36 @@ import React, { Component } from "react";
 import { withAuthenticator } from "aws-amplify-react";
 
 class App extends Component {
+	state = {
+		notes: [
+			{
+				id: 1,
+				note: "Hello World"
+			}
+		]
+	};
 	render() {
-		return <div> App </div>;
+		const { notes } = this.state;
+		return (
+			<div className="flex flex-column items-center justify-center pa3 bg-washed-red">
+				<h1 className="code f2-l">Note Taker</h1>
+				<form className="mb3">
+					<input type="text" className="pa2 f4" placeholder="Write Your Note" />
+					<button className="pa2 f4">Add Note</button>
+				</form>
+				{/* Notes List*/}
+				<div>
+					{notes.map((item) => (
+						<div key={item.id} className="flex items-center">
+							<li className="list pa1 f3">{item.note}</li>
+							<button className="bg-transparent bn f4">
+								<span>&times; </span>
+							</button>
+						</div>
+					))}
+				</div>
+			</div>
+		);
 	}
 }
 
